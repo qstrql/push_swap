5ef40fe8d15c134cdfc5c2276eacb8339f95c764
mjouot <mjouot@student.42angouleme.fr>
2022-10-31 19:54:34 +0100

    ca code ou quoi

--- ft_init_sort.c
+++ ft_init_sort.c
@@ -6,19 +6,19 @@
 /*   By: mjouot <mjouot@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 19:24:33 by mjouot            #+#    #+#             */
-/*   Updated: 2022/10/31 19:27:18 by mjouot           ###   ########.fr       */
+/*   Updated: 2022/10/31 19:54:14 by mjouot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/INCLUDES/libft.h"
 
-/*
+
 void	ft_sort_more(t_stack *sa, t_stack *sb)
 {
 
 }
-*/
+
 void	ft_sort_three(t_stack *sa)
 {
 	if (sa->tab[0] > sa->tab[1] && sa->tab[0] > sa->tab[2])
@@ -44,11 +44,10 @@ int	ft_init_sort(t_stack *sa, t_stack *sb)
 		ft_sort_three(sa);
 		return (1);
 	}
-/*	if (sa->size > 3)
+	if (sa->size > 3)
 	{
 		ft_sort_more(sa, sb);
 		return (1);
 	}
-*/
 	return (0);
 }
